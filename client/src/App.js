4a62ce5a2dfebfc2d1ea88fd1ad4a89ee9ce9857
Mohammed Zaid <mohammedzaid.26.10@gmail.com>
2023-12-16 08:40:28 +0530

    Phase 2 commit

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoute from "./components/Routes/ProtectedRoute";
+import PublicRoute from "./components/Routes/PublicRoute";
+
+function App() {
+  return (
+    <>
+      <ToastContainer /> {/* Place ToastContainer outside of Routes */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
